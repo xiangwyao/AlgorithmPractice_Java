5e7c89f58f616585f9d02b017fa272c672ac6eb4
Wenyao Xiang <xiangwyao@gmail.com>
2017-01-25 23:15:31 -0500

    Build basic tree structures and find potential tree algorithms.

---
+++ TreesImplementation/src/BinaryTreeNode.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Wenyao on 1/24/2017.
+ */
+
+
+public class BinaryTreeNode <T> {
+    // Generics is used
+    // Abstract Data Structure
+
+    public T data;
+    public BinaryTreeNode <T> left;
+    public BinaryTreeNode <T> right;
+}
